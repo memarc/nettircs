18cd0e91f18d35e68a07e4ffecf3556894f44ada
otso <otso>
2020-06-13 15:54:13 +0000

    added an extra variable so the script also coppies the RCS directory

--- nettircs.sh
+++ nettircs.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-if [ -z "$1" -o -z "$2" ]
+if [ -z "$1" -o -z "$2" -o -z "$3" ]
 then
-	echo "Syntax: ./$0 nrcsuser project"
+	echo "Syntax: ./$0 RCS_dir nrcsuser project"
 	exit
 fi
 
 
 PROJECT_PATH=/var/www/htdocs/nettircs/projects/
-cd $PROJECT_PATH/$1/$2
+mv $1 $PROJECT_PATH/$2/$3
+cd $PROJECT_PATH/$2/$3
 rcs -u RCS/*
 [ -e "RCS/README.md,v" ] && co README.md
-tar -czf $2.rcs.tar.gz RCS
+tar -czf $3.rcs.tar.gz RCS
