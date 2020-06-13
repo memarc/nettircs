50451545fdbf7d956328b487ae68de19b2938bdb
otso <otso>
2020-06-13 20:20:37 +0000

    fixed tmp settings so RCS files aren't sitting around

--- nettircs.sh
+++ nettircs.sh
@@ -10,6 +10,7 @@ PROJECT_PATH=/var/www/htdocs/nettircs/projects/
 [ -e $PROJECT_PATH/$2/$3 ] && mkdir -p $PROJECT_PATH/$2/$3
 rm -rf $PROJECT_PATH/$2/$3/RCS
 cp -r $1 $PROJECT_PATH/$2/$3/RCS
+rm -fr $1
 cd $PROJECT_PATH/$2/$3
 rcs -u RCS/*
 [ -e "RCS/README.md,v" ] && co README.md
