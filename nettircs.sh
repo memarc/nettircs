11de27ce3404652978cfc033a1278c374fa3fb52
otso <otso>
2020-06-13 14:45:42 +0000

    Initial revision

---
+++ nettircs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z "$1" -o -z "$2" ]
+then
+	echo "Syntax: ./$0 nrcsuser project"
+	exit
+fi
+
+
+PROJECT_PATH=/var/www/htdocs/nettircs/projects/
+cd $PROJECT_PATH/$1/$2
+[ -e "RCS/README.md,v" ] && co README.md
+tar -czf $2.rcs.tar.gz RCS
